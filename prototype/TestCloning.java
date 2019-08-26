bb5383ec8df2d9f112c630686dccf742523e47d4
Lee Angioletti <langio@outlook.com>
2019-08-26 16:57:02 -0400

    adding prototype project

---
+++ prototype/TestCloning.java
@@ -0,0 +1,20 @@
+package prototype;
+
+public class TestCloning {
+
+	public static void main(String[] args) {
+		CloneFactory animalMaker = new CloneFactory();
+		
+		Sheep sally = new Sheep();
+		
+		Sheep clonedSheep = (Sheep) animalMaker.getClone(sally);
+		
+		System.out.println(sally);
+		System.out.println(clonedSheep);
+
+		
+		System.out.println("Sally hashcode: " + System.identityHashCode(sally));
+		System.out.println("Clone hashcode: " + System.identityHashCode(clonedSheep));
+	}
+
+}
